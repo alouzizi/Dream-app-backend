76333ad35b4462aaac486421c3061b4bc3a765c0
mfagri <90139919+mfagri@users.noreply.github.com>
2024-10-22 15:54:29 +0100

    try fix deployment

--- dream_app_backend/src/main.ts
+++ dream_app_backend/src/main.ts
@@ -34,6 +34,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
